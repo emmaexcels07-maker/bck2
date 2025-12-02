daf2d790144646980411580c2844ebb6078617b0
Emmanuel Feyisayo <emmaexcels07@gmail.com>
2025-12-02 10:32:39 +0100

    Describe the changes made

--- letest/app/page.jsx
+++ letest/app/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { getToken, removeToken } from "../lib/auth.js";
+import { getToken, removeToken, saveToken  } from "./lib/auth.js";
 
 const API_URL = "https://bck2-dtr1.onrender.com/api/v1/todos";
 
