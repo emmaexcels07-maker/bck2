d7194171e6cece8fda4f8742bbeccc21c9171432
Emmanuel Feyisayo <emmaexcels07@gmail.com>
2025-12-03 13:23:17 +0100

    Describe the changes made

--- backend/database/mongodb.js
+++ backend/database/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.DB_URI);
     console.log("MongoDB Connected");
   } catch (error) {
     console.log("Database Error:", error);
