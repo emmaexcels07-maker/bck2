32d96a77347d8fba268c5dcbc830bd3fcbdd95b3
Emmanuel Feyisayo <emmaexcels07@gmail.com>
2025-12-07 19:55:46 +0100

    Describe the changes made

--- letest/app/layout.tsx
+++ letest/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 
-export const metadata = { title: "Shadow App" };
+export const metadata = { title: "NextShop" };
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
