2d629f999871ff8ab4bbe99da7fe10d7097ec78a
Emmanuel Feyisayo <emmaexcels07@gmail.com>
2025-12-14 13:49:39 +0100

    Describe the changes made

--- backend/routes/order.routes.js
+++ backend/routes/order.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { auth, admin } from "../middlewares/auth.js";
+import { auth, admin } from "../middlewares/auth.middleware.js";
 import {
   createOrder,
   getMyOrders,
