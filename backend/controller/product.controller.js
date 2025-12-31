cf851c07a52c8b9b99ade074af72533e779f69d3
Emmanuel Feyisayo <emmaexcels07@gmail.com>
2025-12-31 15:12:26 +0100

    Describe the changes made

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -1,4 +1,4 @@
-import Product from "../models/product";
+import Product from "../models/product.js";
 import cloudinary from "../lib/cloudinary.js";
 
 export async function getProducts(req, res) {
