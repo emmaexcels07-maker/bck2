cc001273eb0a7f2927a362d9528f4c8bb0f83eed
Emmanuel Feyisayo <emmaexcels07@gmail.com>
2026-06-29 11:46:27 +0100

    Describe the changes made

--- backend/index.js
+++ backend/index.js
@@ -2,41 +2,50 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./database/mongodb.js";
-import orderRoutes from "./routes/order.routes.js";
+import compression from "compression";
+import path from "path";
+
+import authRoutes from "./routes/auth.routes.js";
 import productRoutes from "./routes/product.routes.js";
 import cartRoutes from "./routes/cart.routes.js";
-import path from "path";
-import authRoutes from "./routes/auth.routes.js";
-import compression from "compression";
+import orderRoutes from "./routes/order.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
 import shopRoutes from "./routes/product.shop.routes.js";
-
 
 dotenv.config();
 connectDB();
 
 const app = express();
+
+// Middleware
+app.use(compression());
+
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true
 }));
 
 app.use(express.json());
-app.use('/uploads', express.static(path.join(process.cwd(), '/uploads')));
 
+app.use(
+  "/uploads",
+  express.static(path.join(process.cwd(), "uploads"))
+);
+
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/products/shop", shopRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
-app.use(compression());
 app.use("/api/categories", categoryRoutes);
-app.use("/api/products/shop", shopRoutes);
-
-
 
 app.get("/", (req, res) => {
   res.send("E-Commerce Backend Running");
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
