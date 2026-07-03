ddcac91db509c2fa5698842c55c7f6642b38b08a
Emmanuel Feyisayo <emmaexcels07@gmail.com>
2026-07-03 11:55:21 +0100

    Describe the changes made

--- letest/app/signin/page.jsx
+++ letest/app/signin/page.jsx
@@ -39,7 +39,7 @@ export default function SignInPage() {
                 }
 
                 if (res.user?.role === "admin") {
-                    router.push("/admin/AdminClient");
+                    router.push("/admin/page.jsx");
                     return;
                 }
 
